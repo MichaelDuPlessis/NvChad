21ea01cf8865d2dd68d31594e0e70e49a27078ba
Michael du Plessis <52035435+MichaelDuPlessis@users.noreply.github.com>
2023-07-16 19:06:25 +0200

    Moved custom config files

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,18 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+local lspconfig = require "lspconfig"
+
+local servers = { "pyright" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities
+  }
+end
+
+lspconfig["hls"].setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "haskell-language-server-wrapper", "--lsp" },
+}
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,85 @@
+local cmp = require "cmp"
+
+local plugins = {
+  -- lsp servers
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- rust
+        "rust-analyzer",
+        -- python
+        "pyright",
+        "autopep8",
+        "pylint",
+        -- haskell
+        "haskell-language-server",
+      },
+    },
+  },
+  -- lsp
+  {
+    "neovim/nvim-lspconfig",
+    -- format and lint
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  -- rust
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+  -- rust
+  {
+    'saecki/crates.nvim',
+    ft = {"toml"},
+    config = function(_, opts)
+      local crates  = require('crates')
+      crates.setup(opts)
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+      crates.show()
+      require("core.utils").load_mappings("crates")
+    end,
+  },
+  -- rust
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  -- also rust
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      M.completion.completeopt = "menu,menuone,noselect"
+      M.mapping["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = false,
+      }
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  }
+}
+
+return plugins
